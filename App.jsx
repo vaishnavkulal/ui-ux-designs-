99cf2fb01cf1ba21d9cbcee4b7270a4883a264fc
vaishnavkulal <vaishnavkulal.work@gmail.com>
2025-12-18 11:03:37 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Cards from './components/Content'
+
+function App(){
+  return (
+    <div>
+      <Navbar />
+      <Cards />
+    </div>
+  )
+}
+
+export default App
